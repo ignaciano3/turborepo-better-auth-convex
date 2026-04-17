518637de9cc878680933f709f59d27062f65ca80
Ignacio Garcia P <igarciap@fi.uba.ar>
2026-04-16 23:05:24 -0300

    mini mejoras

--- apps/web/src/app/(auth)/sign-in/page.tsx
+++ apps/web/src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Title from "../../../components/Title";
 import { SignInForm } from "@/components/SignInForm";
 
@@ -6,6 +7,9 @@ export default function SignInPage() {
     <div className="flex flex-col items-center justify-center h-screen">
       <Title>Sign In</Title>
       <SignInForm />
+      <Link href="/sign-up" className="mt-4 text-blue-500 hover:underline">
+        Don&apos;t have an account? Sign Up
+      </Link>
     </div>
   );
 }
--- apps/web/src/app/(auth)/sign-up/page.tsx
+++ apps/web/src/app/(auth)/sign-up/page.tsx
@@ -1,12 +1,16 @@
 
 import Title from "@/components/Title";
 import { SignUpForm } from "@/components/SignUpForm";
+import Link from "next/link";
 
 export default function SignUpPage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Title>Sign Up</Title>
       <SignUpForm />
+      <Link href="/sign-in" className="mt-4 text-blue-500 hover:underline">
+        Already have an account? Sign In
+      </Link>
     </div>
   );
 }
